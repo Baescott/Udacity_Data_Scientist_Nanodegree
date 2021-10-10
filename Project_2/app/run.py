24e7018c0d5c50e006d85176ff410c74fa7b2c80
In Bae Kang <kibae92@gmail.com>
2021-10-10 21:55:32 +0900

    add more visualization

--- Project_2/app/run.py
+++ Project_2/app/run.py
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Scatter
 import joblib
 #from sklearn.externals import joblib
 from sqlalchemy import create_engine
@@ -41,32 +41,83 @@ def index():
     
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
+    ## -------------------------------------------
+    ## 1. Dist of Message Genres
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    ## --- make graph
+    graph_one = []
+    graph_one.append(
+                     Bar(
+                         x = genre_names,
+                         y = genre_counts
+                         )
+                     )
+    
+    ## --- make layout
+    layout_one = dict(
+                      title = 'Distribution of Message Genres',
+                      xaxis = dict(title = 'Genre'),
+                      yaxis = dict(title = 'Count')
+                      )
+    
+    ## -------------------------------------------
+    ## 2. Dist of Tags
+    tags_counts = df.iloc[:, 4:].sum().sort_values(ascending=False)
+    tags_names = list(tags_counts.index)
+    
+    ## --- make graph
+    graph_two = []
+    graph_two.append(
+                     Bar(
+                         x = tags_names,
+                         y = tags_counts
+                         )
+                     )
+        
+    ## --- make layout
+    layout_two = dict(
+                      title = 'The Number of Tags; Descending',
+                      xaxis = dict(title = 'Tag'),
+                      yaxis = dict(title = 'Count')
+                      )
+
+    ## -------------------------------------------
+    ## 3. Most Common Words in Message; Top 20
+    stopwords = ['the', 'and', 'for', 'have', 'are', 'with', 'you', 'this', "n't", 'not']
+    word_tmp = []
+    for i in range(len(df['message'])):
+        word_tmp += tokenize(df['message'][i])
+    
+    word_counts = pd.Series([word for word in word_tmp if (len(word)>2) and (word not in stopwords)])
+    word_counts = word_counts.value_counts().sort_values(ascending=False)[:20]
+    word_names = list(word_counts.index)
+
+    ## --- make graph
+    graph_three = []
+    graph_three.append(
+                     Bar(
+                         x = word_names,
+                         y = word_counts
+                         )
+                     )
+
+    ## --- make layout
+    layout_three = dict(
+                      title = 'Most Common Words in Message; Top 20',
+                      xaxis = dict(title = 'Word'),
+                      yaxis = dict(title = 'Count')
+                      )
+    
+    ## -------------------------------------------
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
-    graphs = [
-        {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
+    graphs = []
+    graphs.append(dict(data=graph_one, layout=layout_one))
+    graphs.append(dict(data=graph_two, layout=layout_two))
+    graphs.append(dict(data=graph_three, layout=layout_three))
 
-            'layout': {
-                'title': 'Distribution of Message Genres',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
-        }
-    ]
-    
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
